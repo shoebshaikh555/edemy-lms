a21cb1b83de017650704254787f7af4864fb8dd7
shoebshaikh555 <shoeb.shaikh@5d.solutions>
2025-04-30 18:45:50 +0530

    Modified server.js

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import connectDB from "./configs/mongodb.js";
 import { clerkWebhooks } from "./controllers/webhooks.js";
+import { verifyWebhook } from "@clerk/express/webhooks";
 
 // Initialize Express
 const app = express();
@@ -15,7 +16,35 @@ app.use(cors());
 
 // Routes
 app.get("/", (req, res) => res.send("API Working"));
-app.post("/clerk", express.json(), clerkWebhooks);
+app.post(
+  "/clerk",
+  // express.json(),
+  express.raw({ type: "application/json" }),
+  clerkWebhooks
+);
+// app.post(
+//   "/clerk",
+//   express.raw({ type: "application/json" }),
+//   async (req, res) => {
+//     try {
+//       const evt = await verifyWebhook(req);
+
+//       // Do something with payload
+//       // For this guide, log payload to console
+//       const { id } = evt.data;
+//       const eventType = evt.type;
+//       console.log(
+//         `Received webhook with ID ${id} and event type of ${eventType}`
+//       );
+//       console.log("Webhook payload:", evt.data);
+
+//       return res.send("Webhook received");
+//     } catch (err) {
+//       console.error("Error verifying webhook:", err);
+//       return res.status(400).send("Error verifying webhook");
+//     }
+//   }
+// );
 
 // Port
 const PORT = process.env.PORT || 5000;
