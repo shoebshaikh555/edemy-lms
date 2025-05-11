d6921402399fdd6b7103ef0452994bbd7ff71c7d
shoebshaikh555 <shoebshaikh555@gmail.com>
2025-05-11 06:00:36 +0530

    Backend complete

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,6 +2,7 @@ import Stripe from "stripe";
 import Purchase from "../models/Purchase.js";
 import User from "../models/User.js";
 import Course from "../models/Course.js";
+import CourseProgress from "../models/CourseProgress.js";
 
 // Get User Data
 export const getUserData = async (req, res) => {
@@ -70,7 +71,8 @@ export const purchaseCourse = async (req, res) => {
     ];
 
     const session = await stripeInstance.checkout.sessions.create({
-      success_url: `${origin}/loading/my-enrollments`,
+      // success_url: `${origin}/loading/my-enrollments`,
+      success_url: `${origin}loading/my-enrollments`,
       cancel_url: `${origin}/`,
       line_items,
       mode: "payment",
@@ -79,6 +81,77 @@ export const purchaseCourse = async (req, res) => {
       },
     });
     res.json({ success: true, session_url: session.url });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// Update User Course Progress
+export const updateUserCourseProgress = async (req, res) => {
+  try {
+    const userId = req.auth.userId;
+    const { courseId, lectureId } = req.body;
+    const progressData = await CourseProgress.findOne({ userId, courseId });
+    if (progressData) {
+      if (progressData.lectureCompleted.includes(lectureId)) {
+        res.json({ success: true, message: "Lecture already completed" });
+      }
+      progressData.lectureCompleted.push(lectureId);
+      await progressData.save();
+    } else {
+      await CourseProgress.create({
+        userId,
+        courseId,
+        lectureCompleted: [lectureId],
+      });
+    }
+    res.json({ success: true, message: "Progress updated" });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// Get User Course Progress
+export const getUserCourseProgress = async (req, res) => {
+  try {
+    const userId = req.auth.userId;
+    const { courseId } = req.body;
+    const progressData = await CourseProgress.findOne({ userId, courseId });
+    res.json({ success: true, progressData });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// Add User Ratings to Course
+export const addUserRating = async (req, res) => {
+  const userId = req.auth.userId;
+  const { courseId, rating } = req.body;
+  if (!courseId || !userId || !rating || rating < 1 || rating > 5) {
+    res.json({ success: false, message: "Invalid details" });
+  }
+  try {
+    const course = await Course.findById(courseId);
+    if (!course) {
+      res.json({ success: false, message: "Course not found" });
+    }
+    const user = await User.findById(userId);
+    if (!user || !user.enrolledCourses.includes(courseId)) {
+      return res.json({
+        success: false,
+        message: "User has not purchase this course",
+      });
+    }
+    const existingRatingIndex = course.courseRatings.findIndex(
+      (r) => r.userId === userId
+    );
+    if (existingRatingIndex > -1) {
+      course.courseRatings[existingRatingIndex].rating = rating;
+    } else {
+      course.courseRatings.push({ userId, rating });
+    }
+    await course.save();
+    return res.json({ success: true, message: "Rating added" });
   } catch (error) {
     res.json({ success: false, message: error.message });
   }
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -3,6 +3,9 @@ import {
   getUserData,
   userEnrolledCourses,
   purchaseCourse,
+  updateUserCourseProgress,
+  getUserCourseProgress,
+  addUserRating,
 } from "../controllers/userController.js";
 
 const userRouter = express.Router();
@@ -10,5 +13,8 @@ const userRouter = express.Router();
 userRouter.get("/data", getUserData);
 userRouter.get("/enrolled-courses", userEnrolledCourses);
 userRouter.post("/purchase", purchaseCourse);
+userRouter.post("/update-course-progress", updateUserCourseProgress);
+userRouter.post("/get-course-progress", getUserCourseProgress);
+userRouter.post("/add-rating", addUserRating);
 
 export default userRouter;
